76549c4f92efcc1cebcacad4fa03c886a73ee898
Md. Kowsar Ahmed <129593721+kowsar-ahmed@users.noreply.github.com>
2023-04-02 16:12:38 +0600

    Update series1.c

--- series1.c
+++ series1.c
@@ -2,20 +2,14 @@
 
 int main() {
     // C programme to find sum of the series : "1+2+...+n"
-    // Prompt the user to enter a value for n
-    printf("Enter the value of n: ");
-
-    // Read in the value of n
     int n;
+    printf("Enter the value of n: ");
     scanf("%d", &n);
 
-    // Compute the sum of the series using a for loop
     int sum = 0;
     for (int i = 1; i <= n; i++) {
         sum += i;
     }
-
-    // Print the result
     printf("Sum of the series 1 + 2 + ... + %d = %d\n", n, sum);
 
     return 0;
